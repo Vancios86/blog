c1a2e28ac92d1907735625defd1468e5a96b8122
Vancios86 <albuion86@gmail.com>
2021-11-17 19:10:30 +0000

    uncomplete react interface

---
+++ blog-interface/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.js";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <div id="page-body">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/article" element={<ArticlePage />} />
+          <Route path="/articles-list" element={<ArticlesList />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
